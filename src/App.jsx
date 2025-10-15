e015353ba261457a0fc717b935c9c4b5d1ea3ad4
edoardorizzo <edo96.rizzo@gmail.com>
2025-10-15 22:22:47 +0200

    add style

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,36 @@ import './App.css'
 import weather from './logic/weather'
 
 function App() {
-  const [meteo, setMeteo] = useState("");
+  const [meteo, setMeteo] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchWeather = async () => {
       const result = await weather.doGet();
-      setMeteo(result)
+      setMeteo(result);
+
+      setTimeout(() => {
+        setLoading(false);
+      }, 1500);
     };
 
     fetchWeather();
   }, []);
 
+  if (loading || meteo === null) {
+    return (
+      <div className='loading_wrapper' style={{ backgroundColor: "gray" }}>
+        <h1 className='waiting'>Piove oggi a Lonigo?</h1>
+        <p>Aspetta un attimo...</p>
+      </div>
+    )
+  }
+
   return (
     <div className='weather_wrapper' style={meteo === "Clear" ? { backgroundColor: "green" } : { backgroundColor: "red" }}>
-      {meteo === "Clear" ?
-        <h1>NO</h1> : <h1>SI</h1>}
-    </div >
+      {meteo === "Clear" ? <h1 className='rain_today'>NO</h1> : <h1 className='rain_today'>SI</h1>}
+    </div>
   )
 }
 
-export default App
+export default App;
